561108417d6eb564b8cb58a048594fe259227ae9
Rui <mobileware@iMac-2.local>
2017-04-19 10:56:29 -0400

    Enable verbose log

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+    databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || 'myAppId',
+    masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+    verbose: true,
+    liveQuery: {
+        classNames: ["LiveQueryTestObject", "Message"] // List of classes to support for query subscriptions
+    }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -36,17 +37,17 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+    res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
 app.get('/func1', function (req, res) {
-  res.status(200).send("test api works");
+    res.status(200).send("test api works");
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
 app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+    res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
 var port = process.env.PORT || 1337;
@@ -56,4 +57,4 @@ httpServer.listen(port, function() {
 });
 
 // This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
+ParseServer.createLiveQueryServer(httpServer, {logLevel: 'VERBOSE'});
